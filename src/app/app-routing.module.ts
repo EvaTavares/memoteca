6dd946a4d0775719686bdf0243367da00aa4620e
Eva Tavares <eva.programadora@gmail.com>
2024-05-19 15:21:45 -0300

    feat: Rotas e direcionamentos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateThoughtComponent } from './components/pensamentos/create-thought/create-thought.component';
+import { ListThoughtComponent } from './components/pensamentos/list-thought/list-thought.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listThought',
+    pathMatch: 'full'
+  },
+  {
+    path: 'createThought',
+    component: CreateThoughtComponent
+  },
+  {
+    path: 'listThought',
+    component: ListThoughtComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CreateThoughtComponent } from './components/pensamentos/create-thought/create-thought.component';
 import { FormsModule } from '@angular/forms';
+import { ListThoughtComponent } from './components/pensamentos/list-thought/list-thought.component';
+import { CardThoughtComponent } from './components/pensamentos/card-thought/card-thought.component';
+import { ThoughtComponent } from './components/pensamentos/thought/thought.component';
 
 
 @NgModule({
@@ -15,6 +18,9 @@ import { FormsModule } from '@angular/forms';
     HeaderComponent,
     FooterComponent,
     CreateThoughtComponent,
+    ListThoughtComponent,
+    CardThoughtComponent,
+    ThoughtComponent,
   ],
   imports: [
     BrowserModule,
