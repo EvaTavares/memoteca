0e719bacfe8c442e3645991cedc71d26959915e8
Eva Tavares <eva.programadora@gmail.com>
2024-05-26 20:29:17 -0300

    feat: Layout do componente pai

--- src/app/components/pensamentos/list-thought/list-thought.component.ts
+++ src/app/components/pensamentos/list-thought/list-thought.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-thought.component.css']
 })
 export class ListThoughtComponent implements OnInit {
+  thoughtList = [
+    {
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam assumenda aperiam hic iusto nesciunt. Fuga, error provident cupiditate iusto consequatur, cum voluptatibus veniam temporibus corrupti libero nam quod vero ratione.',
+      authorship: 'Dev',
+      model: 'model1'
+    },
+    {
+      content: 'Passo informações para o componente filho',
+      authorship: 'Componente pai',
+      model: 'model2'
+    },
+    {
+      content: 'Minha propriedade é decorada com o @Input() angular-cor',
+      authorship: 'Componente Filho',
+      model: 'model3'
+    }
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
